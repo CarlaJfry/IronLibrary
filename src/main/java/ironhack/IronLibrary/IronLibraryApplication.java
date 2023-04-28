50c6da17bd7d46903d814006c81e5bce8a263384
Carla G <you@example.com>
2023-04-28 13:09:59 +0200

    first commit

---
+++ src/main/java/ironhack/IronLibrary/IronLibraryApplication.java
@@ -0,0 +1,13 @@
+package ironhack.IronLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ironhack/IronLibrary/IronLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ironhack.IronLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
