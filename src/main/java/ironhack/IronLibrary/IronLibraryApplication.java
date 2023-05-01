5b488b567750e6179494d51d519b7cc75f0b06d3
Rocio Salgado <salgadorocio@hotmail.com>
2023-05-01 12:03:58 +0200

    adds command's methods

--- src/main/java/ironhack/IronLibrary/IronLibraryApplication.java
+++ src/main/java/ironhack/IronLibrary/IronLibraryApplication.java
@@ -1,13 +1,17 @@
 package ironhack.IronLibrary;
 
+import ironhack.IronLibrary.model.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IronLibraryApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(IronLibraryApplication.class, args);
+
+		Menu menu = new Menu();
+		menu.userInteraction();
 	}
 
 }
--- src/main/java/ironhack/IronLibrary/model/Menu.java
+++ src/main/java/ironhack/IronLibrary/model/Menu.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Menu {
     private Boolean exit = false;
     Scanner scanner = new Scanner(System.in);
+
+    private int command;
     private String ironlibraryLogo = " __  .______        ______   .__   __.  __       __  .______   .______          ___      .______     ____    ____ \n" +
             "|  | |   _  \\      /  __  \\  |  \\ |  | |  |     |  | |   _  \\  |   _  \\        /   \\     |   _  \\    \\   \\  /   / \n" +
             "|  | |  |_)  |    |  |  |  | |   \\|  | |  |     |  | |  |_)  | |  |_)  |      /  ^  \\    |  |_)  |    \\   \\/   /  \n" +
@@ -12,6 +14,7 @@ public class Menu {
             "|  | |  |\\  \\----.|  `--'  | |  |\\   | |  `----.|  | |  |_)  | |  |\\  \\----./  _____  \\  |  |\\  \\----.   |  |     \n" +
             "|__| | _| `._____| \\______/  |__| \\__| |_______||__| |______/  | _| `._____/__/     \\__\\ | _| `._____|   |__|";
     public static final String COURSE_IMAGE = "📚";
+    public static final String SAD_FACE = "\uD83D\uDE25";
     public static final String COLOR_GREEN = "\u001B[32m";
     public static final String COLOR_RED = "\u001B[31m";
     public static final String COLOR_RESET = "\u001B[0m";
@@ -23,11 +26,16 @@ public class Menu {
 
     public void userInteraction() throws InterruptedException {
         greeting();
-        displayMenu();
-        introduceCommand();
+        while(exit==false){
+            displayMenu();
+            executeCommands(introduceCommand());
+        }
     }
     public void greeting() throws InterruptedException {
         Thread.sleep(1000);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
         System.out.println(COLOR_PURPLE+ironlibraryLogo+COLOR_RESET);
         Thread.sleep(1000);
         System.out.println("");
@@ -55,18 +63,165 @@ public class Menu {
         System.out.println("║ " + "8. Exit" + "                     " +"                       ║");
         System.out.println("╚════════════════════════════════════════════════════╝" + COLOR_RESET);
     }
-    public String introduceCommand() throws InterruptedException {
+    public int introduceCommand() throws InterruptedException {
         Thread.sleep(1000);
         System.out.println("");
-        System.out.println("Enter a command:");
-        String command = scanner.nextLine();
+        System.out.println("Enter your choice:");
+        command = validateNumber();
         return command;
     }
-    public void executeCommands(String command) {
-        if(command=="1"){
-//              addABook();
-        }else if(command=="2"){
-//                searchBookByTitle();
+    public void executeCommands(int command) throws InterruptedException {
+        if(command==1){
+              addBook();
+        }else if(command==2){
+                searchBookByTitle();
+        }else if(command==3){
+            searchBookByCategory();
+        }else if(command==4){
+            searchBookByAuthor();
+        }else if(command==5){
+            listBooksWithAuthor();
+        }else if(command==6){
+//            issueBook();
+        }else if(command==7){
+//            listBooks();
+        }else if(command==8){
+//            exit();
+        }else{
+            System.out.println("Please enter one of the possible available commands: 1, 2, 3...8");
+            executeCommands(introduceCommand());
         }
+    }
+
+    public void listBooksWithAuthor(){
+        System.out.println("");
+        System.out.println(COURSE_IMAGE+COURSE_IMAGE+COURSE_IMAGE+" Here is your list of books : "+COURSE_IMAGE+COURSE_IMAGE+COURSE_IMAGE);
+        System.out.println("");
+        System.out.println("Title: The Notebook");
+        System.out.println("Category: Romance");
+        System.out.println("Author: Rocio Salgado");
+        System.out.println("");
+
+    }
+    public void searchBookByCategory(){
+        String filter = "category";
+        searchByString(filter);
+    }
+    public void searchBookByTitle(){
+        String filter = "title";
+        searchByString(filter);
+    }
+
+    public void searchBookByAuthor(){
+        String filter = "author";
+        searchByString(filter);
+    }
+
+    public void searchByString(String str){
+        String param;
+        if(str=="title"){
+            System.out.println("Enter title :");
+        }else if(str=="author"){
+            System.out.println("Enter Author Name :");
+        }else{
+            System.out.println("Enter category :");
+        }
+        param=validateString();
+//        SI ENCUENTRA EL BOOK
+        System.out.println("");
+        System.out.println("We found your book! "+COURSE_IMAGE);
+        System.out.println("");
+        System.out.println("Title: The Notebook");
+        System.out.println("Category: Romance");
+        System.out.println("Author: Rocio Salgado");
+//        SI NO ENCUENTRA EL BOOK
+        System.out.println("");
+        System.out.println("");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("---------------------------------Oops! We didn't found your book " + SAD_FACE+ "------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("");
+    }
+    public void addBook(){
+        String isbn;
+        String title;
+        String category;
+        String authorName;
+        int numberOfBooks;
+        System.out.println("Enter isbn :");
+        isbn=validateString();
+        System.out.println("Enter title :");
+        title=validateString();
+        System.out.println("Enter category :");
+        category=validateString();
+        System.out.println("Enter Author name :");
+        authorName=validateName();
+        String email=validateEmail();
+        System.out.println("Enter number of books :");
+        numberOfBooks=validateNumber();
+        System.out.println("");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("------------------------------Great job! We have added your book to our database! "+COURSE_IMAGE+"-------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("");
+    }
+
+    public String validateName(){
+        String name = null;
+        while (name == null || name.isEmpty()) {
+            System.out.println("(minimum of 2 words, letters only)");
+            name = scanner.nextLine();
+            if (!isValidName(name)) {
+                System.out.println("Invalid name. Please enter a name with only letters and at least two words.");
+                name = "";
+            }
+        }return name;
+    }
+
+    private boolean isValidName(String name) {
+        return name.matches("^[A-Za-z]+(\\s[A-Za-z]+)+$");
+    }
+    public String validateEmail() {
+        String email = null;
+        while (email == null || email.isEmpty() || !isValidEmail(email)) {
+            System.out.println("Enter Author mail:");
+            email = scanner.nextLine();
+            if (!isValidEmail(email)) {
+                System.out.println("Invalid email address. Please enter a valid email address.");
+            }
+        }
+        return email;
+    }
+
+    public String validateString() {
+        String input = null;
+        while (input == null || input.isEmpty()) {
+            input = scanner.nextLine();
+            if (input.isEmpty()) {
+                System.out.println("Cannot be empty. Please enter it again.");
+            }
+        }
+        return input;
+    }
+
+    private boolean isValidEmail(String email) {
+        // Regular expression pattern to validate email addresses
+        String pattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return email.matches(pattern);
+    }
+
+    public int validateNumber() {
+        String input = scanner.nextLine();
+        int number;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+            number = validateNumber();
+        }
+        return number;
     }
 }
