685a577808b1cd06607914c7dd1996d034ccd4f4
Carla G <you@example.com>
2023-05-01 18:28:11 +0200

    commands done except 7

--- src/main/java/ironhack/IronLibrary/IronLibraryApplication.java
+++ src/main/java/ironhack/IronLibrary/IronLibraryApplication.java
@@ -1,16 +1,23 @@
 package ironhack.IronLibrary;
 
 import ironhack.IronLibrary.model.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IronLibraryApplication {
+public class IronLibraryApplication implements CommandLineRunner {
+	@Autowired
+	Menu menu;
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(IronLibraryApplication.class, args);
-		Menu menu = new Menu();
-		menu.userInteraction();
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		menu.userInteraction();
+	}
 }
--- src/main/java/ironhack/IronLibrary/model/Student.java
+++ src/main/java/ironhack/IronLibrary/model/Student.java
@@ -32,4 +32,12 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "usn='" + usn + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
